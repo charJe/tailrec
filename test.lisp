618075e8993251e79cbf88d7041319da89c44766
Charles Jackson <cbj170230@utdallas.edu>
2021-02-16 22:29:42 -0600

    Add a simple test

---
+++ test.lisp
@@ -0,0 +1,19 @@
+(tailrec
+ (defun fib (n &optional (a 0) (b 1))
+   "Return the Nth Fibonnaci number."
+   (cond
+     ((= 0 n) a)
+     ((= 1 n) b)
+     (:else (fib (- n 1) b (+ a b))))))
+
+;; this would be better to memoize
+(tailrec
+ (defun fib-bad (n)
+   "Return the Nth Fibonnaci number."
+   (if (< n 2)
+       n
+       (+ (fib-bad (- n 1))
+          (fib-bad (- n 2))))))
+
+(assert (= (fib 10)
+           (fib-bad 10)))
