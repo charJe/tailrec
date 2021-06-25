2389ee5006b9e4961cba3d83839793aa063bc82c
Charles <charles.b.jackson@protonmail.com>
2021-06-24 22:29:49 -0500

    Remove destructuring-bind and duplication

--- tailrec.lisp
+++ tailrec.lisp
@@ -5,6 +5,9 @@
   (:import-from #:trivial-macroexpand-all
                 macroexpand-all)
   (:import-from #:alexandria
+                plist-alist
+                simple-program-error
+                parse-ordinary-lambda-list
                 parse-body)
   (:export deftailrec tailrec nlet))
 (in-package #:tailrec)
@@ -59,9 +62,19 @@ ARGS is the symbol storing the arguments before a jump."
     (multiple-value-bind
           (body declarations documentation)
         (parse-body body :documentation t)
+      (multiple-value-bind
+            (params optional rest keys allow-other-keys-p aux keyp)
+          (parse-ordinary-lambda-list lambda-list)
+        (declare (ignore aux allow-other-keys-p))
       (let* ((start (gensym))
              (result (gensym))
              (args (gensym))
+             (moptional (gensym))
+             (moptional-values (gensym))
+             (mkeys (gensym))
+             (mkey-keys (gensym))
+             (mkey-values (gensym))
+             (mkey-args (gensym))
              (*optimized* nil)
              (body (map 'list 'macroexpand-all body))
              (*last-form* (first (last body)))
@@ -70,21 +83,54 @@ ARGS is the symbol storing the arguments before a jump."
                     (lambda (form)
                       (optimize-tails name start args form))
                     body))
-             (fun `(,name ,lambda-list ,documentation ,declarations
-                          (let ((,result nil)
-                                (,args nil))
-                            (tagbody
-                               ,start
-                               (setq ,result
-                                     (if ,args
-                                         (destructuring-bind ,lambda-list ,args
-                                           ,@optimized)
-                                         (progn ,@optimized))))
-                            ,result))))
+             (fun
+               `(,name
+                 ,lambda-list
+                 ,documentation
+                 ,declarations
+                 (let ((,result nil)
+                       (,args nil)
+                       (,moptional ',(map 'list 'first optional))
+                       (,moptional-values (list ,@(map 'list 'second optional)))
+                       (,mkeys ',(map 'list 'cadar keys))
+                       (,mkey-keys ,(map 'list 'caar keys))
+                       (,mkey-values (list ,@(map 'list 'second keys))))
+                   (declare (ignorable ,moptional ,moptional-values
+                                       ,mkeys ,mkey-keys ,mkey-values))
+                   (tagbody ,start
+                      (setq ,result
+                            (progn
+                              (when ,args
+                                ;; set required args
+                                (mapc 'set ',params ,args)
+                                ;; shift args
+                                (setq ,args (nthcdr ,(length params) ,args))
+                                ;; set optional parameters
+                                ,(when optional
+                                   `(progn
+                                      ;; they might not all be supplied
+                                      (mapc 'set ,moptional ,moptional-values)
+                                      (mapc 'set ,moptional ,args)
+                                      ;; shift args
+                                      (setq ,args (nthcdr ,(length optional) ,args))))
+                                ;; set rest
+                                ,(when rest
+                                   `(setq ,rest ,args))
+                                ;; set keys
+                                ,(when keyp
+                                   `(let ((,mkey-args (plist-alist ,args)))
+                                      (mapc (lambda (param key init)
+                                              (set param (or (cdr (assoc key ,mkey-args))
+                                                             init)))
+                                            ,mkeys
+                                            ,mkey-keys
+                                            ,mkey-values))))
+                              ,@optimized)))
+                   ,result))))
         (cond
           ((not *optimized*) defunition)
           (def (cons def fun))
-          (:else fun))))))
+          (:else fun)))))))
 
 (defmacro deftailrec (name lambda-list &body body)
   "Wrapper for `tailrec' to reduce nesting by one layer."
